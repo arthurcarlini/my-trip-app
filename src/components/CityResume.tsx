1f04f43558bb8d96ce9486e582225a444fd81846
Arthur Carlini <arthurhabowski@outlook.com>
2023-08-09 02:31:46 -0300

    add component CityResume

---
+++ src/components/CityResume.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function CityResume() {
+
+    return (
+        <div className="flex flex-col lg:flex-row lg:justify-between mb-5 lg:mb-10">
+            <h2 className="mb-5 font-bold text-2xl lg:text-4xl"></h2>
+            <p className="lg:w-1/2 lg:font-medium lg:text-lg text-center lg:text-left">Porto Alegre, RS: cidade vibrante às margens do Rio Guaíba, com rica cultura, parques encantadores e gastronomia deliciosa. Mistura de tradição e modernidade, acolhe visitantes com calor humano gaúcho.</p>
+        </div>
+    )
+}
