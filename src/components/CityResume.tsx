1537b58e31294e96f9912a8641451049cea40c37
Arthur Carlini <arthurhabowski@outlook.com>
2023-08-22 16:30:17 -0300

    update cityResume component

--- src/components/CityResume.tsx
+++ src/components/CityResume.tsx
@@ -1,11 +1,15 @@
 'use client'
+import { useApiContext } from "@/app/context/ApiContext"
+
 
 export default function CityResume() {
 
+    const { destinationCity } = useApiContext()
+
     return (
         <div className="flex flex-col lg:flex-row lg:justify-between mb-5 lg:mb-10">
-            <h2 className="mb-5 font-bold text-2xl lg:text-4xl"></h2>
-            <p className="lg:w-1/2 lg:font-medium lg:text-lg text-center lg:text-left">Porto Alegre, RS: cidade vibrante às margens do Rio Guaíba, com rica cultura, parques encantadores e gastronomia deliciosa. Mistura de tradição e modernidade, acolhe visitantes com calor humano gaúcho.</p>
+            <h2 className="mb-5 font-bold text-2xl lg:text-4xl">{destinationCity.name}</h2>
+            <p className="lg:w-1/2 lg:font-medium lg:text-lg text-center lg:text-left"></p>
         </div>
     )
 }
