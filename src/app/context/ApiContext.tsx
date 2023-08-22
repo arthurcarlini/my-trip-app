dc04cd643f9dc010ca3f5885c638e16de7c69a4f
Arthur Carlini <arthurhabowski@outlook.com>
2023-08-22 16:29:07 -0300

    update context

--- src/app/context/ApiContext.tsx
+++ src/app/context/ApiContext.tsx
@@ -4,21 +4,26 @@ import { createContext, useState, useContext, ReactNode } from "react"
 import { Wrapper } from "@googlemaps/react-wrapper"
 
 interface ApiContextType {
-    origin: google.maps.LatLngLiteral | null
-    setOrigin: (value: google.maps.LatLngLiteral | null) => void
-    destination: google.maps.LatLngLiteral | null
-    setDestination: (value: google.maps.LatLngLiteral | null) => void
+    originCity: City
+    setOriginCity: (value: City) => void
+    destinationCity: City
+    setDestinationCity: (value: City) => void
+}
+
+interface City {
+    name: string
+    location: google.maps.LatLngLiteral
 }
 
 const ApiContext = createContext({} as ApiContextType)
 
 export function ApiProvider({ children }: { children: ReactNode }) {
-    const [origin, setOrigin] = useState<google.maps.LatLngLiteral | null>(null)
-    const [destination, setDestination] = useState<google.maps.LatLngLiteral | null>(null)
+    const [originCity, setOriginCity] = useState({} as City)
+    const [destinationCity, setDestinationCity] = useState({} as City)
 
     return (
         <Wrapper apiKey="AIzaSyBxC2hxuRJ-HjyfgIUKekDIw_ndNOd9yQA" version="weekly" libraries={["places"]}>
-            <ApiContext.Provider value={{ origin, setOrigin, destination, setDestination }}>
+            <ApiContext.Provider value={{ originCity, setOriginCity, destinationCity, setDestinationCity }}>
                 {children}
             </ApiContext.Provider>
         </Wrapper>
