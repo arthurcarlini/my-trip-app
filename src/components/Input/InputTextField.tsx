650b6874418e0969b8193a3c76c50cfbcb61b5a1
Arthur Carlini <arthurhabowski@outlook.com>
2023-08-23 18:04:32 -0300

    update input component

--- src/components/Input/InputTextField.tsx
+++ src/components/Input/InputTextField.tsx
@@ -12,38 +12,41 @@ interface DestinationInputType {
 
 export default function InputTextField({ placeholder, onPlacesChange }: DestinationInputType) {
     const inputRef = useRef<HTMLInputElement | null>(null)
+    const searchboxRef = useRef<google.maps.places.SearchBox | null>(null)
 
     useEffect(() => {
-        initSearchBox()
+        if (inputRef.current !== null) {
+            initSearchBox(inputRef.current, onPlacesChange)
+        }
     }, [])
 
-    function initSearchBox() {
-        if (inputRef.current !== null) {
-            const searchbox = new window.google.maps.places.SearchBox(inputRef.current)
-
-            searchbox.addListener("places_changed", () => {
-                const places = searchbox.getPlaces()
-
-                if (places?.length == 0) {
-                    return
-                }
-
-                const place = places![0]
-                const cityName = place.name
-
-                const location = {
-                    lat: place.geometry?.location?.lat() || 0,
-                    lng: place.geometry?.location?.lng() || 0
-                }
-                
-                if (cityName !== undefined) {
-                    onPlacesChange({
-                        name: cityName,
-                        location,
-                    })
-                }
-            })
+    function initSearchBox(input: HTMLInputElement, onPlacesChange: (value: { name: string, location: google.maps.LatLngLiteral }) => void) {
+        if (!searchboxRef.current) {
+            searchboxRef.current = new window.google.maps.places.SearchBox(input)
         }
+
+        searchboxRef.current.addListener("places_changed", () => {
+            const places = searchboxRef.current?.getPlaces()
+
+            if (places?.length == 0) {
+                return
+            }
+
+            const place = places![0]
+            const name = place.name
+
+            const location = {
+                lat: place.geometry?.location?.lat() || 0,
+                lng: place.geometry?.location?.lng() || 0
+            }
+
+            if (name !== undefined) {
+                onPlacesChange({
+                    name,
+                    location,
+                })
+            }
+        })
     }
 
     return (
@@ -51,5 +54,4 @@ export default function InputTextField({ placeholder, onPlacesChange }: Destinat
             <input ref={inputRef} placeholder={placeholder} className="w-1/2 ml-3 text-black text-sm md:text-xl focus:outline-none" type="text" />
         </>
     )
-
 }
