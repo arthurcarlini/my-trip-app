070415ca57ceb845a8727c4fe076e35e1cda5478
Arthur Carlini <arthurhabowski@outlook.com>
2023-09-13 02:15:37 -0300

    update icons and home page image

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -5,5 +5,17 @@ import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded'
 import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded'
 import ExpandLessRoundedIcon from '@mui/icons-material/ExpandLessRounded'
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined'
+import StarRateIcon from '@mui/icons-material/StarRate'
 
-export { MenuRoundedIcon, MoreVertRoundedIcon, ExpandMoreRoundedIcon, ExpandLessRoundedIcon, SearchRoundedIcon }
+export {
+    MenuRoundedIcon,
+    MoreVertRoundedIcon,
+    ExpandMoreRoundedIcon,
+    ExpandLessRoundedIcon,
+    SearchRoundedIcon,
+    ArrowBackIcon,
+    RoomOutlinedIcon,
+    StarRateIcon
+}
