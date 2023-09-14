27d4550de9504aa935b5a1986f89144bf7e0af04
Arthur Carlini <arthurhabowski@outlook.com>
2023-09-14 17:20:45 -0300

    add autocomplete functions to useEffect

--- src/components/Input/autocompleteInput.tsx
+++ src/components/Input/autocompleteInput.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 
 interface DestinationInputType {
     placeholder: string
@@ -16,34 +16,33 @@ export default function InputTextField({ placeholder, setPlaceDetails }: Destina
 
     useEffect(() => {
         initAutocomplete()
-    }, [])
+        function initAutocomplete() {
+            if (inputRef.current != null) {
 
-    function initAutocomplete() {
-        if (inputRef.current != null) {
+                const options = {
+                    fields: ["place_id", "formatted_address"],
+                    type: ["(cities)"]
+                }
 
-            const options = {
-                fields: ["place_id", "formatted_address"],
-                type: ["(cities)"]
+                if (!autocompleteRef.current) {
+                    autocompleteRef.current = new google.maps.places.Autocomplete(inputRef.current, options)
+                }
+
+                autocompleteRef.current.addListener("place_changed", getAndSetPlaceDetails)
             }
-
-            if (!autocompleteRef.current) {
-                autocompleteRef.current = new google.maps.places.Autocomplete(inputRef.current, options)
-            }
-
-            autocompleteRef.current.addListener("place_changed", getAndSetPlaceDetails)
-        }
-    }
-
-    function getAndSetPlaceDetails() {
-        const placeResponse = autocompleteRef.current?.getPlace()
-
-        const place = {
-            placeId: placeResponse?.place_id,
-            address: placeResponse?.formatted_address
         }
 
-        setPlaceDetails(place)
-    }
+        function getAndSetPlaceDetails() {
+            const placeResponse = autocompleteRef.current?.getPlace()
+
+            const place = {
+                placeId: placeResponse?.place_id,
+                address: placeResponse?.formatted_address
+            }
+
+            setPlaceDetails(place)
+        }
+    }, [setPlaceDetails])
 
     return (
         <input
