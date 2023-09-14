c9c2116391d1c8439bab361369f3a7a452b6bb48
Arthur Carlini <arthurhabowski@outlook.com>
2023-09-14 17:14:59 -0300

    update accordion

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,42 +1,35 @@
 'use client'
+
 import { useState } from "react"
 import AccordionPanel from "./AccordionPanel"
-import imageTest from "../../../public/porto-alegre-3.jpg"
 
-export default function AccordionRoot() {
+interface Place {
+    id: string | undefined
+    name: string | undefined
+    address: string | undefined
+    photos: string | undefined
+    rating: number | undefined
+}
+
+interface AccordionRootType {
+    attractions: Place[]
+}
+
+export default function AccordionRoot({ attractions }: AccordionRootType) {
     const [activeIndex, setActiveIndex] = useState<number>(0)
 
-    const image = imageTest
     return (
         <div className="mt-2 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-2">
-            <AccordionPanel
-                isActive={activeIndex === 0}
-                show={() => setActiveIndex(0)}
-                img={image}
-                title={"Praça"}
-                text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ipsam asperiores alias illum hic consequatur molestiae quia doloribus architecto minima minus velit, libero ullam enim tenetur perspiciatis sunt labore quidem."}
-            />
-            <AccordionPanel
-                isActive={activeIndex === 1}
-                show={() => setActiveIndex(1)}
-                img={image}
-                title={"Cinema"}
-                text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ipsam asperiores alias illum hic consequatur molestiae quia doloribus architecto minima minus velit, libero ullam enim tenetur perspiciatis sunt labore quidem."}
-            />
-            <AccordionPanel
-                isActive={activeIndex === 2}
-                show={() => setActiveIndex(2)}
-                img={image}
-                title={"Cinema"}
-                text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ipsam asperiores alias illum hic consequatur molestiae quia doloribus architecto minima minus velit, libero ullam enim tenetur perspiciatis sunt labore quidem."}
-            />
-            <AccordionPanel
-                isActive={activeIndex === 3}
-                show={() => setActiveIndex(3)}
-                img={image}
-                title={"Cinema"}
-                text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ipsam asperiores alias illum hic consequatur molestiae quia doloribus architecto minima minus velit, libero ullam enim tenetur perspiciatis sunt labore quidem."}
-            />
+            {attractions.map(attraction => (
+                <AccordionPanel
+                    key={attraction.id}
+                    isActive={activeIndex === attractions.indexOf(attraction)}
+                    show={() => setActiveIndex(attractions.indexOf(attraction))}
+                    photo={attraction.photos}
+                    placeName={attraction.name}
+                    text={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ipsam asperiores alias illum hic consequatur molestiae quia doloribus architecto minima minus velit, libero ullam enim tenetur perspiciatis sunt labore quidem."}
+                />
+            ))}
         </div>
     )
 }
