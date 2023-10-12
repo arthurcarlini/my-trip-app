e24ea4f401576d55e77b0c4d5686778e3ffb414a
Arthur Carlini <arthurhabowski@outlook.com>
2023-10-12 19:19:18 -0300

    add google api key

--- src/app/context/ApiContext.tsx
+++ src/app/context/ApiContext.tsx
@@ -5,7 +5,8 @@ import { Wrapper } from "@googlemaps/react-wrapper"
 
 export function ApiProvider({ children }: { children: ReactNode }) {
     return (
-        <Wrapper apiKey="AIzaSyBxC2hxuRJ-HjyfgIUKekDIw_ndNOd9yQA" version="weekly" libraries={["places"]}>
+        //@ts-ignore
+        <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY} version="weekly" libraries={["places"]}>
             {children}
         </Wrapper>
     )
