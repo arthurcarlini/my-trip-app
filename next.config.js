7b83458ff63f39090cc0d629b0d2e0903cec98ef
Arthur Carlini <arthurhabowski@outlook.com>
2023-10-15 23:52:00 -0300

    update remote patterns

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'maps.googleapis.com',
+                port: '',
+                pathname: '/maps/api/place/**',
             },
         ],
     },
