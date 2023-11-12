a5cda82256948a90dd452dc511180ff1a2abef8d
Arthur Carlini <arthurhabowski@outlook.com>
2023-11-12 01:55:40 -0300

    remove modal css from Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,10 +4,8 @@ import { useState, FormEvent } from "react"
 import { useRouter } from "next/navigation"
 
 import InputTextField from "./autocompleteInput"
-import { SearchRoundedIcon, CloseIcon } from "../ui/icons"
 
-interface Input {
-    show: boolean
+interface InputType {
     setShow: (value: boolean) => void
 }
 
@@ -16,7 +14,7 @@ interface Place {
     address: string | undefined
 }
 
-export default function Input({ show, setShow }: Input) {
+export default function Input({ setShow }: InputType) {
     const [origin, setOrigin] = useState({} as Place)
     const [destination, setDestination] = useState({} as Place)
 
@@ -28,39 +26,23 @@ export default function Input({ show, setShow }: Input) {
         router.push(`/place?origin=${origin.placeId}&destination=${destination.placeId}`)
     }
 
-    if (show) {
-        document.body.style.overflow = "hidden"
-    } else {
-        document.body.style.overflow = "auto"
-    }
-
     return (
-        <div className={`${show ? "block" : "hidden"} fixed inset-0 lg:px-96 lg:py-40 bg-transparent backdrop-blur-md`}>
-            <div className="h-full lg:h-auto p-5 bg-white rounded-lg">
-                <button
-                    className="h-10"
-                    onClick={() => setShow(false)}>
-                    <CloseIcon />
-                </button>
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <InputTextField
+                label="Origem"
+                placeholder="New York, USA"
+                setPlaceDetails={setOrigin}
+            />
 
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <InputTextField
-                        label="Origem"
-                        placeholder="New York, USA"
-                        setPlaceDetails={setOrigin}
-                    />
+            <InputTextField
+                label="Destino"
+                placeholder="Washington, USA"
+                setPlaceDetails={setDestination}
+            />
 
-                    <InputTextField
-                        label="Destino"
-                        placeholder="Washington, USA"
-                        setPlaceDetails={setDestination}
-                    />
-
-                    <button className="w-20 h-10 p-2 transition-colors border border-neutral-400 hover:border-neutral-500 rounded-md">
-                        Buscar
-                    </button>
-                </form>
-            </div>
-        </div>
+            <button className="w-20 h-10 p-2 transition-colors border border-neutral-400 hover:border-neutral-500 rounded-md">
+                Buscar
+            </button>
+        </form>
     )
 }
